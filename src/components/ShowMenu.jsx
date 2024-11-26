c097e3214a405b472f545bd7f151e1814b078263
Zeeshan <zeeshanmalik.1011@gmail.com>
2024-11-26 12:18:26 +0500

    forth  comment

--- src/components/ShowMenu.jsx
+++ src/components/ShowMenu.jsx
@@ -4,7 +4,7 @@ const ShowMenu = () => {
   return (
     <>
       <div className=" relative   ">
-        <div className="flex justify-center bg-black w-1/3 py-5 rounded-lg  absolute  right-3  text-white">
+        <div className="flex justify-center bg-black w-3/4 md:w-1/3 py-5 rounded-lg  absolute  right-3  text-white">
           <ul className="  text-center space-y-4 ">
             <li>
               <button className="font-semibold">Home</button>
